064b596f1d42bf3a57a002585cc3f23b56d14530
plupyt <55117123+plupyt@users.noreply.github.com>
2020-01-20 10:56:00 -0600

    Clase login con constructor y una función

--- ADA3. Diseño de software/src/CSV/Login.java
+++ ADA3. Diseño de software/src/CSV/Login.java
@@ -5,10 +5,41 @@
  */
 package CSV;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 /**
  *
  * @author plupy
  */
 public class Login {
+    private String user;
+    private String password;
+    ArrayList<ArrayList<String>> datosUsers;
+
+    public Login(String user, String password, ArrayList<ArrayList<String>> datosUsers) {
+        this.user = user;
+        this.password = password;
+        this.datosUsers = datosUsers;
+    }
     
+    private static void readCSV(ArrayList<ArrayList<String>> data){
+        try{
+            BufferedReader br =new BufferedReader(new FileReader("C:\\Users\\jimmy\\Documents\\NetBeansProjects\\ADA3.-Dise-o-de-software\\ListaAlumnos.csv"));
+            String line = br.readLine();
+            while (line != null){
+                String[] dataLine = line.split(",");
+                ArrayList<String> tempData = new ArrayList<String>();
+                for (String dato : dataLine){
+                    tempData.add(dato);
+                }
+                data.add(tempData);
+                line = br.readLine();
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 }
