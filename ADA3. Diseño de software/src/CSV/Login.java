aabc223b696c22e94dbf77057631ce25b439825e
plupyt <55117123+plupyt@users.noreply.github.com>
2020-01-20 11:18:34 -0600

    login con constructor modificado y comparación de función

--- ADA3. Diseño de software/src/CSV/Login.java
+++ ADA3. Diseño de software/src/CSV/Login.java
@@ -5,10 +5,11 @@
  */
 package CSV;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
+import org.apache.commons.codec.binary.Hex;
+
 
 /**
  *
@@ -19,16 +20,52 @@ public class Login {
     private String password;
     ArrayList<ArrayList<String>> datosUsers;
 
-    public Login(String user, String password, ArrayList<ArrayList<String>> datosUsers) {
+    
+    //Constructor
+    public Login(String user, String password, ArrayList<ArrayList<String>> datosUsers) throws NoSuchAlgorithmException {
         this.user = user;
         this.password = password;
         this.datosUsers = datosUsers;
-        
+        try{
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            md.update(password.getBytes());
+            byte[] mb = md.digest();
+            System.out.println(Hex.encodeHex(mb));
+        }
+        catch(NoSuchAlgorithmException e){
+            System.out.println("Error en el constructor del login");
+        }
+    }
+
+    //funciones get
+    public String getUser() {
+        return user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public ArrayList<ArrayList<String>> getDatosUsers() {
+        return datosUsers;
+    }
+
+    //funciones set
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setDatosUsers(ArrayList<ArrayList<String>> datosUsers) {
+        this.datosUsers = datosUsers;
     }
     
+    //demas funciones
     public boolean comparePass(){
         return true;
     }
-    
     
 }
